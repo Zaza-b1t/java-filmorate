f4f2d3faf451b2a993c0f1826012744f4ffa183d
Zaza <ilyatyuleneff@yandex.ru>
2025-08-27 12:40:41 +0300

    Исправлены ошибки с Checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.validator.Filmvalidator;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
